fd12bf8facc37abaf3f16351fbc51acb001ac4d3
k.rantou <kallirant@gmail.com>
2020-11-11 13:16:34 +0200

    remove dependecies about MariaDb and H2

--- src/main/java/com/kalli/Controller/StudentController.java
+++ src/main/java/com/kalli/Controller/StudentController.java
@@ -21,7 +21,7 @@ import java.util.regex.Pattern;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.multipart.MultipartFile;
-import sun.security.mscapi.CPublicKey;
+//import sun.security.mscapi.CPublicKey;
 
 
 @RestController
